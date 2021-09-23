026034036e1934107b733eb068813bae1c0e0abf
ccartwright123 <ccartwright@qa.com>
2021-09-23 14:02:22 +0100

    FP-20 #comment DTO of bankcard

---
+++ project/src/main/java/com/bae/project/dto/BankcardDTO.java
@@ -0,0 +1,54 @@
+package com.bae.project.dto;
+
+public class BankcardDTO {
+
+	private int bankcardId;
+	private long cardNumber;
+	private String sortCode;
+	private int bankAccountId;
+	private int accountNumber;
+	private String bank;
+	
+	public BankcardDTO() {
+		super();
+	}
+	
+	public int getBankcardId() {
+		return bankcardId;
+	}
+	public void setBankcardId(int bankcardId) {
+		this.bankcardId = bankcardId;
+	}
+	public long getCardNumber() {
+		return cardNumber;
+	}
+	public void setCardNumber(long cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public String getSortCode() {
+		return sortCode;
+	}
+	public void setSortCode(String sortCode) {
+		this.sortCode = sortCode;
+	}
+	public int getBankAccountId() {
+		return bankAccountId;
+	}
+	public void setBankAccountId(int bankAccountId) {
+		this.bankAccountId = bankAccountId;
+	}
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getBank() {
+		return bank;
+	}
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+	
+	
+}
