56b64bef44518c63d92dbfca599a4aa0085cd843
ccartwright123 <ccartwright@qa.com>
2021-09-23 11:40:29 +0100

    FP-21 #comment repo created with findByNameIgnoreCase list

--- project/src/main/java/com/bae/project/data/repo/CitizenRepo.java
+++ project/src/main/java/com/bae/project/data/repo/CitizenRepo.java
@@ -1,5 +1,11 @@
 package com.bae.project.data.repo;
 
-public class CitizenRepo {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.bae.project.data.Citizen;
+
+public interface CitizenRepo extends JpaRepository<Citizen, Integer>{
+
+	List<Citizen> findByNameIgnoreCase(String name);
 }
