82799b515f5f909ecc024549a0330aaf165e6238
ccartwright123 <ccartwright@qa.com>
2021-09-23 11:11:25 +0100

    FP-20 #comment AtmPoint and PeopleBankAccount

---
+++ project/src/main/java/com/bae/project/data/AtmPoint.java
@@ -0,0 +1,90 @@
+package com.bae.project.data;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class AtmPoint {
+
+	@Id
+	private int atmId;
+	private String operator;
+	private String streetName;
+	private String postcode;
+	private double latitude;
+	private double longitude;
+	
+	
+	public AtmPoint(int atmId, String operator, String streetName, String postcode, double latitude, double longitude) {
+		super();
+		this.atmId = atmId;
+		this.operator = operator;
+		this.streetName = streetName;
+		this.postcode = postcode;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	public AtmPoint() {
+		super();
+	}
+
+	public int getAtmId() {
+		return atmId;
+	}
+	public void setAtmId(int atmId) {
+		this.atmId = atmId;
+	}
+	public String getOperator() {
+		return operator;
+	}
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	public String getStreetName() {
+		return streetName;
+	}
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(atmId, latitude, longitude, operator, postcode, streetName);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AtmPoint other = (AtmPoint) obj;
+		return atmId == other.atmId && Double.doubleToLongBits(latitude) == Double.doubleToLongBits(other.latitude)
+				&& Double.doubleToLongBits(longitude) == Double.doubleToLongBits(other.longitude)
+				&& Objects.equals(operator, other.operator) && Objects.equals(postcode, other.postcode)
+				&& Objects.equals(streetName, other.streetName);
+	}
+	
+	
+}
---
+++ project/src/main/java/com/bae/project/data/PeopleBankAccount.java
@@ -0,0 +1,97 @@
+package com.bae.project.data;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class PeopleBankAccount {
+
+	@Id
+	private int bankAccountId;
+	private int accountNumber;
+	private String bank;
+	private String forenames;
+	private String surname;
+	private String dateOfBirth;
+	private String homeAddress;
+	
+	
+	public PeopleBankAccount() {
+		super();
+	}
+	public PeopleBankAccount(int bankAccountId, int accountNumber, String bank, String forenames, String surname,
+			String dateOfBirth, String homeAddress) {
+		super();
+		this.bankAccountId = bankAccountId;
+		this.accountNumber = accountNumber;
+		this.bank = bank;
+		this.forenames = forenames;
+		this.surname = surname;
+		this.dateOfBirth = dateOfBirth;
+		this.homeAddress = homeAddress;
+	}
+	public int getBankAccountId() {
+		return bankAccountId;
+	}
+	public void setBankAccountId(int bankAccountId) {
+		this.bankAccountId = bankAccountId;
+	}
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getBank() {
+		return bank;
+	}
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+	public String getForenames() {
+		return forenames;
+	}
+	public void setForenames(String forenames) {
+		this.forenames = forenames;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getHomeAddress() {
+		return homeAddress;
+	}
+	public void setHomeAddress(String homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(accountNumber, bank, bankAccountId, dateOfBirth, forenames, homeAddress, surname);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PeopleBankAccount other = (PeopleBankAccount) obj;
+		return accountNumber == other.accountNumber && Objects.equals(bank, other.bank)
+				&& bankAccountId == other.bankAccountId && Objects.equals(dateOfBirth, other.dateOfBirth)
+				&& Objects.equals(forenames, other.forenames) && Objects.equals(homeAddress, other.homeAddress)
+				&& Objects.equals(surname, other.surname);
+	}
+	
+	
+}
