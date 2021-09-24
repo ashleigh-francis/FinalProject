a263a72c608fd335d76d144700fac530c1e70c05
ccartwright123 <ccartwright@qa.com>
2021-09-24 09:58:48 +0100

    FP-22 #comment database format

--- project/src/main/java/com/bae/project/rest/CitizenController.java
+++ project/src/main/java/com/bae/project/rest/CitizenController.java
@@ -1,5 +1,20 @@
 package com.bae.project.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bae.project.service.CitizenService;
+
+@RestController
+@CrossOrigin
 public class CitizenController {
 
+	private CitizenService service;
+	
+	public CitizenController(CitizenService service) {
+		super();
+		this.service = service;
+	}
+	
+	
 }
